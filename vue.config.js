6cc553cefcb30eaa7e8b17409120d4a05f4e00ec
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2021-03-22 12:53:48 +0700

    '.'

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	pages: {
 		index: {
 		    // entry for the page
-		    entry: 'src/index/main.js',
+		    entry: 'src/views/index/main.js',
 		    // the source template
 		    template: 'public/index.html',
 		    // output as dist/index.html
