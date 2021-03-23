f55606689e7017b61bacf33d06a2b54b2ad73af7
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2021-03-23 20:01:17 +0700

    Login to host

--- src/views/host/views/Dashboard.vue
+++ src/views/host/views/Dashboard.vue
@@ -2,24 +2,41 @@
 	<v-app>
 		<h1>Halaman Dashboard</h1>
 		<a href="/">Home</a>
+		<a href="#" @click="logOut()">Logout</a>
 	</v-app>
 </template>
 
 <script>
 	export default{
-	
+
 		computed: {
 			//
 		},
-	
+
 		methods: {
-			//
+			logOut: function()
+			{
+				const _token = sessionStorage.getItem('_token')
+				this.axios.get(this.API_URL + '/auth/logout', {
+					params: {
+						token: _token
+					}
+				})
+				.then( () => {
+					sessionStorage.removeItem('_token')
+					window.location.href = '/login'
+				})
+			},
+			checkToken: function()
+			{
+				//
+			}
 		},
-	
+
 		mounted() {
-			//
+			this.checkToken()
 		},
-	
+
 		data() {
 			return {
 				//
