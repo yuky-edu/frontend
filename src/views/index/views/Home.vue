5dbd70191126c1a730f40c11bd684598d82dbfae
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2021-03-29 10:58:26 +0700

    Instant push

--- src/views/index/views/Home.vue
+++ src/views/index/views/Home.vue
@@ -28,7 +28,11 @@
     </div> -->
   </header>
 
-  <section id="about">
+  <section>
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+  </section>
+
+  <!-- <section id="about">
     <div class="header">
       <div class="title">
         <h2 class="sub-title">Kenalin Nih Yuky</h2>
@@ -134,7 +138,7 @@
       </footer>
 
     </div>
-  </section>
+  </section> -->
 
 </div>
 </template>
--- src/views/index/views/components/Navbar.vue
+++ src/views/index/views/components/Navbar.vue
@@ -1,35 +1,66 @@
 <template lang="html">
-  <nav class="navbar navbar-light navbar-expand-md navbar navbar-expand-lg fixed-top" id="mainNav">
-    <div class="nav-container">
-      <a class="navbar-brand" href="#">
+  <div class="">
+    <!-- <nav class="navbar navbar-light navbar-expand-md navbar navbar-expand-lg fixed-top" id="mainNav" style="margin-top: 200px;">
+      <div class="nav-container">
+        <a class="navbar-brand" href="#">
+          <img src="/assets/img/logo.svg">
+        </a>
+        <button data-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-target="#navbarResponsive"
+          type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="nav navbar-nav ml-auto">
+
+            <li v-for="(v, i) in nav" class="nav-item text-light nav-link nav-a">
+              <a class="nav-link active text-light" :href="v.link">{{ v.label }}</a>
+            </li>
+
+            <li class="nav-item nav-link button" role="presentation">
+              <router-link :to="{ name: 'Login'}">
+                <button class="btn btn-nav btn-outline-light btn-login">Masuk</button>
+              </router-link>
+            </li>
+
+            <li class="nav-item nav-link" role="presentation">
+              <router-link :to="{ name: 'Register'}">
+                <button class="btn btn-nav btn-outline-light btn-register">Daftar</button>
+              </router-link>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav> -->
+
+    <nav class="navbar navbar-dark navbar-expand-xl fixed-top">
+      <div class="container">
+        <a class="navbar-brand logo" href="#">
         <img src="/assets/img/logo.svg">
       </a>
-      <button data-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-target="#navbarResponsive"
-        type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="nav navbar-nav ml-auto">
+      <button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="nav navbar-nav ml-auto">
+            <li v-for="(v, i) in nav" class="nav-item text-light nav-link nav-a">
+              <a class="nav-link active text-light" :href="v.link">{{ v.label }}</a>
+            </li>
 
-          <li v-for="(v, i) in nav" class="nav-item text-light nav-link nav-a">
-            <a class="nav-link active text-light" :href="v.link">{{ v.label }}</a>
-          </li>
+            <li class="nav-item nav-link button" role="presentation">
+              <router-link :to="{ name: 'Login'}">
+                <button class="btn btn-nav btn-outline-light btn-login">Masuk</button>
+              </router-link>
+            </li>
 
-          <li class="nav-item nav-link button" role="presentation">
-            <router-link :to="{ name: 'Login'}">
-              <button class="btn btn-nav btn-outline-light btn-login">Masuk</button>
-            </router-link>
-          </li>
-
-          <li class="nav-item nav-link" role="presentation">
-            <router-link :to="{ name: 'Register'}">
-              <button class="btn btn-nav btn-outline-light btn-register">Daftar</button>
-            </router-link>
-          </li>
-
-        </ul>
+            <li class="nav-item nav-link" role="presentation">
+              <router-link :to="{ name: 'Register'}">
+                <button class="btn btn-nav btn-outline-light btn-register">Daftar</button>
+              </router-link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+
+  </div>
 </template>
 
 <script>
@@ -49,11 +80,22 @@ export default {
 
   data() {
     return {
-      nav: [
-        {label: 'Beranda', link: '#'},
-        {label: 'Tentang', link: '#'},
-        {label: 'Tutorial', link: '#'},
-        {label: 'Kontak', link: '#'},
+      nav: [{
+          label: 'Beranda',
+          link: '#'
+        },
+        {
+          label: 'Tentang',
+          link: '#'
+        },
+        {
+          label: 'Tutorial',
+          link: '#'
+        },
+        {
+          label: 'Kontak',
+          link: '#'
+        },
       ]
     }
   },
