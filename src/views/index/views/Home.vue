eb6eef2705ad298bf3ce5ac57561f62b46725b60
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2021-03-29 11:32:58 +0700

    Instant push

--- src/views/index/views/Home.vue
+++ src/views/index/views/Home.vue
@@ -28,11 +28,7 @@
     </div> -->
   </header>
 
-  <section>
-    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
-  </section>
-
-  <!-- <section id="about">
+  <section id="about">
     <div class="header">
       <div class="title">
         <h2 class="sub-title">Kenalin Nih Yuky</h2>
@@ -138,7 +134,7 @@
       </footer>
 
     </div>
-  </section> -->
+  </section>
 
 </div>
 </template>
--- src/views/index/views/components/Navbar.vue
+++ src/views/index/views/components/Navbar.vue
@@ -1,38 +1,5 @@
 <template lang="html">
-  <div class="">
-    <!-- <nav class="navbar navbar-light navbar-expand-md navbar navbar-expand-lg fixed-top" id="mainNav" style="margin-top: 200px;">
-      <div class="nav-container">
-        <a class="navbar-brand" href="#">
-          <img src="/assets/img/logo.svg">
-        </a>
-        <button data-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-target="#navbarResponsive"
-          type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="nav navbar-nav ml-auto">
-
-            <li v-for="(v, i) in nav" class="nav-item text-light nav-link nav-a">
-              <a class="nav-link active text-light" :href="v.link">{{ v.label }}</a>
-            </li>
-
-            <li class="nav-item nav-link button" role="presentation">
-              <router-link :to="{ name: 'Login'}">
-                <button class="btn btn-nav btn-outline-light btn-login">Masuk</button>
-              </router-link>
-            </li>
-
-            <li class="nav-item nav-link" role="presentation">
-              <router-link :to="{ name: 'Register'}">
-                <button class="btn btn-nav btn-outline-light btn-register">Daftar</button>
-              </router-link>
-            </li>
-
-          </ul>
-        </div>
-      </div>
-    </nav> -->
-
-    <nav class="navbar navbar-dark navbar-expand-xl fixed-top">
+    <nav class="navbar navbar-dark navbar-expand-xl fixed-top" :class="{'scroll': navScroll}">
       <div class="container">
         <a class="navbar-brand logo" href="#">
         <img src="/assets/img/logo.svg">
@@ -40,8 +7,8 @@
       <button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="nav navbar-nav ml-auto">
-            <li v-for="(v, i) in nav" class="nav-item text-light nav-link nav-a">
-              <a class="nav-link active text-light" :href="v.link">{{ v.label }}</a>
+            <li v-for="(v, i) in nav" class="nav-item nav-link nav-a">
+              <a class="nav-link active" :href="v.link">{{ v.label }}</a>
             </li>
 
             <li class="nav-item nav-link button" role="presentation">
@@ -59,8 +26,6 @@
         </div>
       </div>
     </nav>
-
-  </div>
 </template>
 
 <script>
@@ -75,7 +40,17 @@ export default {
   },
 
   mounted() {
-    //
+
+    $(window).scroll(function() {
+      const scroll = $(window).scrollTop()
+      // window.console.log(this.navScroll)
+      if (scroll > 100)
+        this.navScroll = true
+      else
+        this.navScroll = false
+    })
+
+
   },
 
   data() {
@@ -96,7 +71,8 @@ export default {
           label: 'Kontak',
           link: '#'
         },
-      ]
+      ],
+      navScroll: true,
     }
   },
 }
