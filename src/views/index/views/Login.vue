260cded180a7eef14f70c69df91090e0bba931df
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2021-04-01 01:11:48 +0700

    Instant push

--- src/views/index/views/Login.vue
+++ src/views/index/views/Login.vue
@@ -6,11 +6,11 @@
         <div class="y-wrapper">
 
           <div class="row">
-            <div class="col-6 text-left">
+            <div class="col-6 text-right">
               <button class="btn btn-outline-light" name="button">Masuk</button>
             </div>
-            <div class="col-6 text-right">
-              <button class="btn btn-outline-light" name="button">Daftar</button>
+            <div class="col-6 text-left">
+              <button class="btn btn-outline-light border-transparent" name="button">Daftar</button>
             </div>
           </div>
 
@@ -20,17 +20,60 @@
           </div>
 
           <div class="back">
-            <a href="#">
+            <router-link :to="{name: 'Home'}">
               <span class="fa fa-arrow-left"></span>
               <p>Kembali ke Beranda</p>
-            </a>
+            </router-link>
           </div>
 
         </div>
       </div>
       <div class="col-md-7 col-lg-8 y-panel-right">
+        <div class="y-wrapper">
 
+          <div class="row">
+            <div class="col-sm-6 offset-sm-3">
+              <div class="external-auth">
+                <div class="wrapper d-flex">
+                  <div class="with-google">
+                    <button class="btn btn-outline-dark">
+                      <span class="fa fa-user"></span>
+                      <span>Login With Google</span>
+                    </button>
+                  </div>
+                  <div class="with-facebook">
+                    <button class="btn" name="button">
+                      <i class="fa fa-facebook"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
 
+              <div class="line-space">
+                <span>Atau</span>
+              </div>
+
+              <div class="main-auth">
+                <form action="/action_page.php">
+                  <div class="form-group">
+                    <input type="email" class="form-control" placeholder="example@yuky.com">
+                  </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Password">
+                  </div>
+                  <div class="form-group form-check">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="checkbox"> Remember me
+                    </label>
+                  </div>
+                  <button class="btn btn-primary">Masuk</button>
+                </form>
+                <p>Lupa Password? <a href="#">Klik Disini</a></p>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div> <!-- ./row -->
   </div>
