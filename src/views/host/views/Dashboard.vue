5199931a882b99958451acf83fd88e644682f15f
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2021-04-04 19:36:12 +0700

    Instant push

--- src/views/host/views/Dashboard.vue
+++ src/views/host/views/Dashboard.vue
@@ -124,22 +124,23 @@
                       </tbody>
                     </table>
                   </div>
-                  <!-- <div class="row mt-5">
-                    <div class="col-md-6 align-self-center">
-                      <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Showing 1 to 10 of 27</p>
-                    </div>
-                    <div class="col-md-6">
-                      <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
+                </div>
+                <div class="card-footer">
+                  <div class="row">
+                    <div class="col-md-6 offset-6">
+                      <div class="d-lg-flex justify-content-lg-end">
                         <ul class="pagination">
-                          <li class="page-item disabled"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+                          <li class="page-item disabled">
+                            <a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a>
+                          </li>
                           <li class="page-item active"><a class="page-link" href="#">1</a></li>
                           <li class="page-item"><a class="page-link" href="#">2</a></li>
                           <li class="page-item"><a class="page-link" href="#">3</a></li>
                           <li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
                         </ul>
-                      </nav>
+                      </div>
                     </div>
-                  </div> -->
+                  </div>
                 </div>
               </div>
             </div>
@@ -164,6 +165,7 @@
     </div>
   </div>
 
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, aliquid cum accusantium iusto, enim adipisci nam. Doloremque beatae perspiciatis perferendis esse qui, optio sequi incidunt illo sapiente aliquam. Alias, odio!</p>
 </div>
 </template>
 
