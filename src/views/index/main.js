ef77399b8d1ede8376ce817e0153a20ac65cbe68
ARSAN <arsandev@users.noreply.github.com>
2021-04-11 23:29:35 +0700

    add vue-cookies

--- src/views/index/main.js
+++ src/views/index/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Nprogress from 'nprogress'
+import vueCookies from 'vue-cookies'
 import router from './router'
 import store from './store'
 
 require('./variable')
 require('./style/main.scss').default
+
+Vue.use(vueCookies)
 Vue.prototype.axios = require('axios').default
 Vue.prototype.Nprogress = Nprogress
 
