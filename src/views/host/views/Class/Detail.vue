ef080784958932fa8583dc722f2366a0eda63faa
ARSAN <arsandev@users.noreply.github.com>
2021-04-15 21:51:22 +0700

    test

--- src/views/host/views/Class/Detail.vue
+++ src/views/host/views/Class/Detail.vue
@@ -56,13 +56,9 @@ export default {
 
   computed: {
     yClass: function() {
-      const data = this.$store.getters[this.Xyclass.g[1]] // data
-      console.log(data)
-      console.log(data)
-      console.log(data)
-      console.log(data)
-      console.log(data)
-      return data
+      const datas = this.$store.getters[this.Xyclass.g[1]] // data
+      console.log(datas)
+      return datas
     }
   },
 
